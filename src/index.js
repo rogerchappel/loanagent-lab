abec3e359929a7ce59c80e6242888c205392c1cd
Roger Chappel <miscanalysis@gmail.com>
2026-05-06 18:36:19 +1000

    feat: expose public library api

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export { inspect } from './lab.js';
+export { loadApplication, listFixtureDirs } from './fixtures.js';
+export { decideApplication } from './decision.js';
+export { scoreApplication, calculateMetrics } from './scoring.js';
+export { buildReport, renderMarkdown } from './report.js';
+export { reviewerChecklist } from './checklist.js';
+export { traceSchema, validateApplication } from './schema.js';
