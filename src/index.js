9bffa7890e7c84e21f923f14ced54f66f9551f3f
Roger Chappel <miscanalysis@gmail.com>
2026-07-31 19:40:52 +1000

    fix: enforce application and CLI contracts

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ export { decideApplication } from './decision.js';
 export { scoreApplication, calculateMetrics } from './scoring.js';
 export { buildReport, renderMarkdown } from './report.js';
 export { reviewerChecklist } from './checklist.js';
-export { traceSchema, validateApplication } from './schema.js';
+export { APPLICATION_LIMITS, traceSchema, validateApplication } from './schema.js';
